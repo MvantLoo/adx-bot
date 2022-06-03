de47ead60f6b72fcb05182d956630f2df4f98a37
MvantLoo <maurice@mvantloo.nl>
2022-06-03 17:11:11 +0200

    Update CRONme.sh
    Remove redirection, improve cron example, so no execute permissions are needed

--- CRONme.sh
+++ CRONme.sh
@@ -2,7 +2,7 @@
 cd ~/adx-bot
 
 # Start the script if it had not generated any output for longer then 60 minutes
-[[ $(date +%s -r adx_bot_status.txt) -lt $(date +%s --date="60 min ago") ]] && python3 adx_bot.py >> adx_bot.log.txt
+[[ $(date +%s -r adx_bot_status.txt) -lt $(date +%s --date="60 min ago") ]] && python3 adx_bot.py
 
 # CRON example to run every hour:
-# 20 * * * * ~/adx-bot/CRONme.sh
+# 0 * * * * bash ~/adx-bot/CRONme.sh
