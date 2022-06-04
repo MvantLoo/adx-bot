67c118835487c7ea68c19aef2127b0ec5f5f8e04
MvantLoo <maurice@mvantloo.nl>
2022-06-04 11:07:45 +0200

    Update Py3c_create.py
      File "/home/mvantloo/binance-spot/Py3c_create.py", line 62
        if not any(x in markets[key]["id"] for x in ["BULL", "BEAR", "HALF", "HEDGE"]) and markets[key]["type"] == "spot" and markets[key]["quote"] == config.QUOTE:
        ^
    IndentationError: expected an indented block

--- Py3c_create.py
+++ Py3c_create.py
@@ -59,9 +59,9 @@ def build_tc_pairs_list(pairs):
             if "PERP" in markets[key]["id"] and not any(perp in markets[key]["id"] for perp in config.PAIRS_BLACKLIST):
                 tc_pairs[markets[key]["id"]] = ""
         else:
-        if not any(x in markets[key]["id"] for x in ["BULL", "BEAR", "HALF", "HEDGE"]) and markets[key]["type"] == "spot" and markets[key]["quote"] == config.QUOTE:
-             if markets[key]["base"] in config.SPOT_COINS:
-                  tc_pairs[markets[key]["id"]] = f'{markets[key]["quote"]}_{markets[key]["base"]}'
+            if not any(x in markets[key]["id"] for x in ["BULL", "BEAR", "HALF", "HEDGE"]) and markets[key]["type"] == "spot" and markets[key]["quote"] == config.QUOTE:
+                if markets[key]["base"] in config.SPOT_COINS:
+                    tc_pairs[markets[key]["id"]] = f'{markets[key]["quote"]}_{markets[key]["base"]}'
     return tc_pairs
 
 
